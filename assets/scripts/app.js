c55a8e25639a2de75733b7b2af0476646f7706b4
juan3178 <juan3178316@gmail.com>
2025-02-14 12:56:38 -0500

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+/*var a;
+var b;
+var c;
+
+a = prompt("ingrese un numero: ", "");
+b = prompt("ingrese otro numero: ", "");
+c = parseInt(a) + parseInt(b);
+
+if (c>0) {
+	document.write("La suma es: "+c+"\n");
+	document.write("Numero positivo");
+}
+if (c<0) {
+	document.write("La suma es: "+ c);
+	document.write("Numero negativo");
+}
+if (c=0) {
+	document.write("La suma es: "+ c);
+	document.write("Numero cero");
+}*/
